162e0e09904feb82494b154fa83ede8b83efb838
bomday <dayanecamilelima@gmail.com>
2023-04-16 23:54:59 -0300

    casting examples

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //CASTING IMPLÍCITO
+        int a = 10;
+        double b = a;
+
+        //CASTING EXPLÍCITO
+        double x = 10.5;
+        int y = (int) x;
+    }
+}
